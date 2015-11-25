4d08b1fdc954ee47ae3b930234e2c583d4d2df75
Joe Lind <joelind@gmail.com>
2015-11-25 03:57:24 -0500

    Fix bug in ActionView rendering

--- lib/awesome_print/ext/action_view.rb
+++ lib/awesome_print/ext/action_view.rb
@@ -8,7 +8,7 @@ module AwesomePrint
 
     # Use HTML colors and add default "debug_dump" class to the resulting HTML.
     def ap_debug(object, options = {})
-      object.ai(options.merge(:html => true)).sub(/^<pre([\s>])/, '<pre class="debug_dump"\\1')
+      ::ActiveSupport::SafeBuffer.new object.ai(options.merge(:html => true)).sub(/^<pre([\s>])/, '<pre class="debug_dump"\\1')
     end
 
     alias_method :ap, :ap_debug
